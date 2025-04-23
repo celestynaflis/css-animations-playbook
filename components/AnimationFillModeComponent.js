f3ad80f81b903815c261af96e39c8c88c6754b48
celestynaflis <celestyna.flis@gmail.com>
2025-04-23 13:41:11 +0200

    Refactor - cleaning up

--- components/AnimationFillModeComponent.js
+++ components/AnimationFillModeComponent.js
@@ -5,7 +5,6 @@ const AnimationFillModeComponent = () => {
     return (
         <div className={styles.container}>
             <p className={`${styles.text} ${styles.forwards}`}>FORWARDS</p>
-
             <p className={`${styles.text} ${styles.backwards}`}>BACKWARDS</p>
         </div>
     );
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,4 @@
-import Image from 'next/image';
 import Layout from '../components/Layout';
-import Hello from '../components/Hello';
 
 export default function Home() {
     return (
